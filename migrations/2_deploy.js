7e4820a4b4a75d1159ce08361ada95df6dca3b78
Petr Kosikhin <mq3_storm@mail.ru>
2019-11-02 21:46:16 -0700

    add migrations for contracts deployment

--- migrations/2_deploy.js
+++ migrations/2_deploy.js
@@ -1,6 +1,11 @@
 const KittyOwnership = artifacts.require("KittyOwnership.sol");
 const PlayerRepo = artifacts.require('./PlayerRepo.sol');
+const ItemOwnership = artifacts.require('./ItemOwnership.sol');
+const Battle = artifacts.require('./Battle.sol');
 
 module.exports = async function(deployer) {
   await deployer.deploy(KittyOwnership);
+  await deployer.deploy(ItemOwnership);
+  await deployer.deploy(PlayerRepo, KittyOwnership.address, ItemOwnership.address);
+  await deployer.deploy(Battle, PlayerRepo.address);
 };
